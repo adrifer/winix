bebbc897eb668d9f697a9e663053649dd54afc1b
Ela <ela@adrifer.com>
2026-05-20 10:51:21 -0700

    fix: wire bundled types via package-level index.d.ts
    Triple-slash references in src/ don't survive tsc emit.
    Instead, create a root index.d.ts that:
    1. References all bundled type augmentations via /// <reference path>
    2. Re-exports dist/index.js types
    
    Point package.json 'types' and exports['.'].types to this file.
    TypeScript auto-loads bundled augmentations when resolving 'winix' imports.
    
    Users don't need to manually include bundled types in their tsconfig.

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="./types/bundled/nixos.d.ts" />
+/// <reference path="./types/bundled/home-manager.d.ts" />
+/// <reference path="./types/bundled/darwin.d.ts" />
+
+export * from "./dist/index.js";
